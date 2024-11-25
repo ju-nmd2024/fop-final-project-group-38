3940edd58962f6dc0c34fc48b9194d1131377b16
ElassyOfficial <ludvigca@outlook.com>
2024-11-25 17:14:40 +0100

    Very simple movement
    Made a super simple movement feature where the map itself moves instead of the player-character. Creating the illusion of having the character moving while still staying in the center of the screen

--- game.js
+++ game.js
@@ -1,7 +1,48 @@
-function setup() {
-  createCanvas(800, 600);
+let mapYposition = 240; //Yposition of everything in the map
+let mapXposition = 300; //Xposition of everything in the map
+let gameRunning = true; //medans man kör är denna true.
+
+function setup() 
+{
+  createCanvas(600, 480);
 }
 
-function draw() {
-  background(255, 140, 0);
+function player()
+{
+  fill(0);
+  ellipse(300, 240, 30, 30);
+}
+
+function map(x, y)
+{
+  fill (50);
+  rect (x + 100, y, 200, 120);
+}
+
+function draw() 
+{
+  background(100);
+
+  
+  map(mapXposition, mapYposition);
+  player();
+
+
+  if (keyIsDown(87)) //KeyCode for "w"
+  {
+    mapYposition += 4;
+  }
+  if (keyIsDown(65)) //KeyCode for "w"
+  {
+    mapXposition += 4;
+  }
+  if (keyIsDown(83)) //KeyCode for "w"
+  {
+    mapYposition -= 4;
+  }
+  if (keyIsDown(68)) //KeyCode for "w"
+  {
+    mapXposition -= 4;
+  }
+
 }
