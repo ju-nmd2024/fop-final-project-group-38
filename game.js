6dea4023c4c710f2f6715f85c882e18b36c5aedd
ElassyOfficial <ludvigca@outlook.com>
2024-12-04 23:34:50 +0100

    Main menu and Map Layout
    Made a simple map layout with the pages spread throughout the rooms.
    
    Also made states for when the game is running or is in main menu. With a simple main menu added as well.
    
    Also added a small page counter in the top left corner that increases when the player collects a page

--- game.js
+++ game.js
@@ -8,6 +8,8 @@ let walls = []; //array for all the walls
 let pages = []; //array for the pages (objects to collect)
 let pagescollected = 0; //keeps track of how many pages are collected
 
+let gameState = "menu"; //determines whether the player is in the menu or playing
+
 noStroke();
 
 function setup()
@@ -20,8 +22,35 @@ function setup()
   walls.push({ x: -300, y: 120, w: 600, h: 50 });
   walls.push({ x: -300, y: -160, w: 80, h: 300 }); 
   walls.push({ x: 220, y: -180, w: 80, h: 330 });
+  walls.push({ x: 450, y: -780, w: 80, h: 260 });
   walls.push({ x: -300, y: -340, w: 600, h: 80 });
+  walls.push({ x: 300, y: -180, w: 200, h: 50 });
+  walls.push({ x: 450, y: -430, w: 80, h: 300 });
+  walls.push({ x: 250, y: -660, w: 80, h: 400 });
+  walls.push({ x: -300, y: -660, w: 80, h: 400 });
+  walls.push({ x: -400, y: -180, w: 200, h: 50 });
+  walls.push({ x: -470, y: -480, w: 80, h: 350 });
+  walls.push({ x: -470, y: -750, w: 80, h: 200 });
+  walls.push({ x: -300, y: -660, w: 600, h: 80 });
+  walls.push({ x: -470, y: -830, w: 500, h: 80 });
+  walls.push({ x: 130, y: -830, w: 400, h: 80 });
+  walls.push({ x: 250, y: -1000, w: 80, h: 200 });
+  walls.push({ x: -100, y: -1000, w: 80, h: 200 });
+  walls.push({ x: -100, y: -1050, w: 430, h: 80 });
+  walls.push({ x: -600, y: -400, w: 200, h: 80 });
+  walls.push({ x: -600, y: -700, w: 200, h: 80 });
+  walls.push({ x: -670, y: -700, w: 80, h: 380 });
+
+  walls.push({ x: 500, y: -400, w: 300, h: 80 });
+  walls.push({ x: 500, y: -700, w: 300, h: 80 });
+  walls.push({ x: 750, y: -700, w: 80, h: 380 });
+
+  //pages created in the "pages" array
   pages.push({ x: -150, y: 50});
+  pages.push({ x: 200, y: -880});
+  pages.push({ x: 0, y: -960});
+  pages.push({ x: 560, y: -580});
+  pages.push({ x: -560, y: -450});
 
 }
 
@@ -31,8 +60,45 @@ function player()
   ellipse(300, 240, playerRadius * 2, playerRadius * 2);
 }
 
-function drawMap(x, y)
+function draw()
 {
+  if (gameState === "menu")
+  {
+    drawMenu();
+  }
+  else if (gameState === "running")
+  {
+    drawGame();
+  }
+  else if (gameState === "gameover")
+  {
+
+  }
+}
+
+function drawMenu()
+{
+  background(40);
+
+  fill(300);
+  rect(180, 70, 250, 360);
+
+  fill(250, 50, 50);
+  textSize(45);
+  text("The Pages", 195, 150);
+
+  textSize(20);
+  text("Press [SPACE] to play", 210, 300);
+
+  fill(300, 10, 10);
+  ellipse(0, 0, 250);
+  ellipse(600, 500, 200);
+
+  
+}
+
+function drawMap(x, y)
+{ 
   push();
   translate(x, y);
   fill (50);
@@ -49,12 +115,9 @@ function drawMap(x, y)
   pop();
 }
 
-function draw()
+function drawGame()
 {
   background(100);
-  textSize(30);
-  fill(0);
-  text(pagescollected + " / 5", 10, 10, 500);
   
   let moveX = 0;
   let moveY = 0;
@@ -75,8 +138,16 @@ function draw()
   }
 
   //allows the player to pick up the pages on the ground
-  checkPageCollision();
+  checkPageCollision(); 
 
+  if(pagescollected === 5)
+  {
+    console.log("YOU WON"); 
+  }
+
+  textSize(30);
+  fill(0);
+  text(pagescollected + " / 5", 10, 10, 500);
 }
 
 //function to check collision with all walls whenever the player moves
@@ -108,7 +179,7 @@ function isColliding(moveX, moveY) {
 //function to check for collision with pages
 function checkPageCollision() {
   let playerX = 300; //player is always centered
-  let playerY = 240;
+  let playerY = 240; 
 
   //loop through pages to check collision
   for (let i = pages.length - 1; i >= 0; i--) {
@@ -122,8 +193,20 @@ function checkPageCollision() {
 
     if (abs(distanceX) < playerRadius + 10 && abs(distanceY) < playerRadius + 15) {
       //collision detected, remove page
-      pages.splice(i, 1); 
+      pages.splice(i, 1);
       pagescollected += 1;
+    }
+  }
+}
+
+//function for leaving main menu using the space key
+function keyPressed() 
+{
+  if(gameState === "menu")
+  {
+    if(key === " ")
+    {
+      gameState = "running";
     }
   }
 }
